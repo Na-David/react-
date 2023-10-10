22cf44e59858cd7782da77611503fd4430ac14d0
David Na <hna206@naver.com>
2023-10-11 00:00:56 +1300

    onCreate - new todos, setTodos, useRef

--- todos-/src/components/TodoEditor.jsx
+++ todos-/src/components/TodoEditor.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./TodoEditor.css";
 
 export default function TodoEditor({ onCreate }) {
   const [todo, setTodo] = useState("");
+  const inputRef = useRef();
 
   const onClick = () => {
+    if (todo === "") {
+      inputRef.current.focus();
+      return;
+    }
     onCreate(todo);
+    setTodo("");
   };
 
   const onChangeTodo = (e) => {
@@ -14,7 +20,12 @@ export default function TodoEditor({ onCreate }) {
 
   return (
     <div className="TodoEditor">
-      <input value={todo} onChange={onChangeTodo} placeholder="New Todo..." />
+      <input
+        ref={inputRef}
+        value={todo}
+        onChange={onChangeTodo}
+        placeholder="New Todo..."
+      />
       <button onClick={onClick}>Add</button>
     </div>
   );
