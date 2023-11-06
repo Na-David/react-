03b62edad63222a55fe4bec2d252a5cd6da44f50
David Na <hna206@naver.com>
2023-11-06 23:06:37 +1300

    useReducer -- reducer(state,action)

--- src/exam/B.jsx
+++ src/exam/B.jsx
@@ -1,9 +1,26 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  return 100;
+}
+
 export default function B() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
     <div>
-      <h4></h4>
+      <h4>{count}</h4>
       <div>
-        <button>-</button>
+        <button
+          onClick={() => {
+            dispatch({
+              type: "DECREASE",
+              data: 1,
+            }); // 상태변화를 유발만 시킴., 어떤 상태변화를 진행할껀지에 대한 인수를 전달해야함--객체로전달
+          }}
+        >
+          -
+        </button>
         <button>+</button>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import A from "./exam/A.jsx";
+import B from "./exam/B.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <App />
-  <A />
+  <B />
 );
