0152f51ff4fca77038e8242be1986a982b75215a
David Na <hna206@naver.com>
2023-11-13 11:13:05 +1300

    Analysing todos

--- todos-/src/components/TodoList.jsx
+++ todos-/src/components/TodoList.jsx
@@ -10,20 +10,34 @@ export default function TodoList({ todos, onUpdate, onDelete }) {
   };
 
   const filterTodos = () => {
-    // if (search === "") {
-    //   return todos;
-    // }
+    if (search === "") {
+      return todos;
+    }
     return todos.filter((todo) =>
       todo.content.toLowerCase().includes(search.toLowerCase())
     );
   };
 
+  const getAnalyzedTodoData = () => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  };
+
+  const { totalCount, doneCount, notDoneCount } = getAnalyzedTodoData();
+
   return (
     <div className="TodoList">
       <h4>Todos</h4>
+      <div></div>
       <input value={search} onChange={onChangeSearch} placeholder="Search.." />
       <div className="todos_wrapper">
-        {filterTodos.map((todo) => (
+        {filterTodos().map((todo) => (
           <TodoItem
             key={todo.id}
             {...todo}
