9227fa06e04e823a1fd8f73b8adacaca1156a64f
David Na <hna206@naver.com>
2023-11-13 11:27:12 +1300

    useMemeo

--- todos-/src/components/TodoList.jsx
+++ todos-/src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import "./TodoList.css";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 export default function TodoList({ todos, onUpdate, onDelete }) {
   const [search, setSearch] = useState("");
@@ -29,12 +29,25 @@ export default function TodoList({ todos, onUpdate, onDelete }) {
     };
   };
 
-  const { totalCount, doneCount, notDoneCount } = getAnalyzedTodoData();
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
 
   return (
     <div className="TodoList">
       <h4>Todos</h4>
-      <div></div>
+      <div>
+        <div>Total: {totalCount}</div>
+        <div>Done: {doneCount}</div>
+        <div>Not Yet Done: {notDoneCount}</div>
+      </div>
       <input value={search} onChange={onChangeSearch} placeholder="Search.." />
       <div className="todos_wrapper">
         {filterTodos().map((todo) => (
