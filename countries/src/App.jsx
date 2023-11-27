f8866bcea8852027b2ac12d0074540c5ad6b9fa6
David Na <hna206@naver.com>
2023-11-27 16:58:22 +1300

    404

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Search from "./pages/Search";
 import Home from "./pages/Home";
 import Country from "./pages/Country";
+import NotFound from "./pages/NotFount";
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/search" element={<Search />} />
       <Route path="/country" element={<Country />} />
+      <Route path="*" element={<NotFound />} />
+      {/*  *is wildcard */}
     </Routes>
   );
 }
