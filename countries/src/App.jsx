9f450ec005ccd25fbecc53804d55709e8fffc6f6
David Na <hna206@naver.com>
2023-11-27 16:46:25 +1300

    routes // route

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Search from "./pages/Search";
+import Home from "./pages/Home";
+import Country from "./pages/Country";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/country" element={<Country />} />
+    </Routes>
+  );
 }
 
 export default App;
