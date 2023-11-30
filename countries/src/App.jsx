bcfe62f0ae6b60ac262c1d8d5928f54dcd63870b
David Na <hna206@naver.com>
2023-11-30 22:06:27 +1300

    query string

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Search from "./pages/Search";
 import Home from "./pages/Home";
 import Country from "./pages/Country";
@@ -7,13 +7,20 @@ import NotFound from "./pages/NotFount";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/country" element={<Country />} />
-      <Route path="*" element={<NotFound />} />
-      {/*  *is wildcard */}
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/country/:code" element={<Country />} />
+        <Route path="*" element={<NotFound />} />
+        {/*  *is wildcard */}
+      </Routes>
+      <div>
+        <Link to={"/"}> Home </Link>
+        <Link to={"/search"}> Search </Link>
+        <Link to={"/country"}> Country </Link>
+      </div>
+    </>
   );
 }
 
