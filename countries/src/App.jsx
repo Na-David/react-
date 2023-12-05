71c4bf8ca256ea3c827063103226d0315b08ca07
David Na <hna206@naver.com>
2023-12-05 21:26:15 +1300

    reset

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,30 +1,19 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Search from "./pages/Search";
 import Home from "./pages/Home";
 import Country from "./pages/Country";
 import NotFound from "./pages/NotFount";
 
 function App() {
-  const nav = useNavigate();
-  const onClick = () => {
-    nav("/search");
-  };
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/country/:code" element={<Country />} />
-        <Route path="*" element={<NotFound />} />
-        {/*  *is wildcard */}
-      </Routes>
-      <div>
-        <Link to={"/"}> Home </Link>
-        <Link to={"/search"}> Search </Link>
-        <Link to={"/country"}> Country </Link>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/country/:code" element={<Country />} />
+      <Route path="*" element={<NotFound />} />
+      {/*  *is wildcard */}
+    </Routes>
   );
 }
 
