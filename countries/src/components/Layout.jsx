9528eb47d343eebde5b263ba49aed826cc9fc90a
David Na <hna206@naver.com>
2023-12-05 22:09:48 +1300

    layout header control

--- countries/src/components/Layout.jsx
+++ countries/src/components/Layout.jsx
@@ -1,3 +1,13 @@
+import style from "./Layout.module.css";
+
 export default function Layout({ children }) {
-  return <div className="Layout">{children}</div>;
+  return (
+    <div className="Layout">
+      <header>
+        <div> 🌏 World Database</div>
+        {/* ctrl+cmd+space=emoji     */}
+      </header>
+      <main>{children}</main>
+    </div>
+  );
 }
