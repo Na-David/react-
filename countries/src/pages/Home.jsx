935a6f5cf2350932b749ef4ac47345e9ad52e56c
David Na <hna206@naver.com>
2023-12-11 16:15:49 +1300

    axios - GET API

--- countries/src/pages/Home.jsx
+++ countries/src/pages/Home.jsx
@@ -1,3 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+async function fetchCountries() {
+  const response = await axios.get("https://naras-api.vercel.app/all");
+  //기본적인 API함수들은 비동기이다. 따라서 response에 API의 값이 언제 저장될지는 모르기 때문에
+  //async, await을 설정해줌으로써 동기적으로 바꿔준 후 사용을 한다.
+  console.log(response);
+}
+
 export default function Home() {
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
   return <div>Home</div>;
 }
