6d55b362d0d404df0328a2fbe74b971620951599
David Na <hna206@naver.com>
2023-12-11 17:22:10 +1300

    search API

--- countries/src/pages/Home.jsx
+++ countries/src/pages/Home.jsx
@@ -1,12 +1,5 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-
-async function fetchCountries() {
-  const response = await axios.get("https://naras-api.vercel.app/all");
-  //기본적인 API함수들은 비동기이다. 따라서 response에 API의 값이 언제 저장될지는 모르기 때문에
-  //async, await을 설정해줌으로써 동기적으로 바꿔준 후 사용을 한다.
-  return response.data;
-}
+import { fetchCountries } from "../api";
 
 export default function Home() {
   const [countires, setCountries] = useState([]);
--- countries/src/pages/Search.jsx
+++ countries/src/pages/Search.jsx
@@ -1,7 +1,20 @@
 import { useSearchParams } from "react-router-dom";
+import { fetchSearchResults } from "../api";
+import { useEffect, useState } from "react";
 
 export default function Search() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const q = searchParams.get("q");
 
-  return <div>Search {searchParams.get("a")}</div>;
+  const [countries, setCountries] = useState([]);
+  const setInitData = async () => {
+    const data = await fetchSearchResults(q);
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    setInitData();
+  }, [q]);
+
+  return <div>Search {searchParams.get("q")}</div>;
 }
