4af08b1a1c8ae662a54d2681973f9cd8e92674f7
David Na <hna206@naver.com>
2024-01-04 23:16:20 +1300

    browsing

--- next/pages/_document.js
+++ next/pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+// 전체 페이지의 메타테그, 캐릭터셋, 폰트 같은것들을 설정해줄 수 있슴 -- 패이지 전체에 할당되는 내용들 담당
 export default function Document() {
   return (
     <Html lang="en">
