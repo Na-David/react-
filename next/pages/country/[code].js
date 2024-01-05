78516e0e42a7c1bb2d88c0a09aa8132867e95432
David Na <hna206@naver.com>
2024-01-05 13:46:55 +1300

    URL Object

--- next/pages/country/[code].js
+++ next/pages/country/[code].js
@@ -4,6 +4,7 @@ export default function Country(){
 
 const router = useRouter();
 const code = router.query.code;
+// const {code} = router.query;
 
     return <div>COuntry detail == {code}</div>
 }
--- next/pages/index.js
+++ next/pages/index.js
@@ -1,4 +1,31 @@
+import Link from "next/link"; //Link는 넥스트에서 제공하는 컴포넌트로, SSR이 아닌 클라이언트가 컴포넌트만 바꿔쓰고싶을때 간단하게 사용할 수 있슴
+import { useRouter } from "next/router";
 
 export default function Home() {
-  return <div>Home</div>
+
+  const code = "KOR";
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/search");
+  }
+
+  return <div>
+    Home
+
+    <div>
+      <button onClick={onClickButton}>GO to Search Page</button>
+    </div>
+
+    <div>
+      <Link href={"/search"}>Search page</Link>
+    </div>
+    <div>
+      <Link href={`/country/${code}`}>Go to {code}</Link> 
+      {/* <Link href = {{
+        pathname: "/country/[code]",
+        query: {code : code}
+      }} 이렇게 작성해도 됌 == URL object 로 구조화 한것*/}
+    </div>
+  </div>
 }
