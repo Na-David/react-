681096e078d0a93569e17df0b81e22ef8228d4b9
David Na <hna206@naver.com>
2024-01-07 23:07:39 +1300

    Search API

--- next/pages/search.js
+++ next/pages/search.js
@@ -1,15 +1,25 @@
 import { fetchSearchResults } from "@/api";
 import SubLayout from "@/components/SubLayout"
 
-export default function Search() {
+export default function Search({countries}) {
     return (
-
+        <div>
+            {countries.map((country) => (<div key={country.code}>{country.commonName}</div>))}
+        </div>
     )
 }
 
 Search.Layout = SubLayout;
 
-export const getServerSideProps = async() =>{
+export const getServerSideProps = async(context) => {
 
+    const {q} = context.query;
+    let countries = [];
+    if(q) {
+        countries = await fetchSearchResults(q)
+    }
 
-};
+    return {
+        props: {countries}
+    }
+}
