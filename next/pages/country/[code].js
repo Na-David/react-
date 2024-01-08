8f18c08f559882f6d6b2544aeb9e9a6ff067e24f
David Na <hna206@naver.com>
2024-01-08 13:14:05 +1300

    error fixed :: country detail

--- next/pages/country/[code].js
+++ next/pages/country/[code].js
@@ -1,13 +1,30 @@
+import { fetchCountry } from "@/api";
 import SubLayout from "@/components/SubLayout";
 import { useRouter } from "next/router"
 
-export default function Country(){
+export default function Country({country}){
 
-const router = useRouter();
-const code = router.query.code;
-// const {code} = router.query;
+    const router = useRouter();
+    //const code = router.query.code;
+     const {code} = router.query;
+    console.log(country);
+    return (
+        <div>{country.commonName}, {country.officialName}</div>
 
-    return <div>COuntry detail == {code}</div>
+    )
 }
 
 Country.Layout = SubLayout;
+
+export const getServerSideProps = async(context) => {
+
+const {code} = context.params;
+    let country = [];
+    if(code) {
+        country = await fetchCountry(code);
+    }
+
+    return {
+        props: {country}
+    }
+}
